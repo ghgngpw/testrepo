3fa15a3ac60ead5693db8edc6c207dc03d2b0b0e
ghgngpw <gngpw@yahoo.com>
2017-09-22 10:31:02 -0400

    Adding feature 2 (#2)
    * Adding feature 2
    
    * Fixing feature2 implementation
    
    Squashing two commits related to feature2 and merge it into master

--- main.py
+++ main.py
@@ -4,8 +4,14 @@
 def feature1_func():
     print('Feature 1')
 
+# Feature 2 implementation
+def feature2_func():
+    print('Feature 2')
+
 print('Hello World')
 print('Hello World')
 print('Hello World')
 
 feature1_func()
+
+feature2_func()
